7595359dc6bc233453508ae1e7b84e4eb111a293
sena <sena@lichtlabs.org>
2025-11-14 05:14:10 +0700

    chore: add necessary development deps on devenv.nix, write todos on README.md to piss me of.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Minimal video repurposing implementation
+
+TODOS:
+
+- [ ] Handle user inputs. For now identified inputs are YouTube Video URL, selected categories, and User Prompt.
+- [ ] YouTube Downloader (using yt-dlp)
+- [ ] Audio transcription, given the extracted audio from the downloaded video (ffmpeg & openai/whisper involved).
+- [ ] Moments / gold-nuggets identification & analysis. For now just raw-dog to Google's Gemini AI begging for trend, viral, moment identification.
+- [ ] Object, subject, and active-speaker detection (TalkNet-ASD) focusing on face, voice, and movement.
+- [ ] Editing & Rendering Clips (oh shi, ffmpeg again...). For captions, emoji, filters, and transitions.
--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,27 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  # lib,
+  # config,
+  # inputs,
+  ...
+}:
 
 {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.nodejs # node
+    pkgs.uv # python development toolchain
+    pkgs.ffmpeg # v/a utilities
+  ];
 
   # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  languages.rust.enable = true;
+  languages.python.enable = true;
+  languages.typescript.enable = true;
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
@@ -40,7 +53,7 @@
   '';
 
   # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks.hooks.rustfmt.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
